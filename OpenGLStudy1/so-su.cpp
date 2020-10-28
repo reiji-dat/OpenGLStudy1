d6eb8d8c6942a7b954a7ee830a8fc8965622286a
Takahashi <66713747+reiji-dat@users.noreply.github.com>
2020-10-28 22:38:15 +0900

    一人称視点、移動

--- OpenGLStudy1/so-su.cpp
+++ OpenGLStudy1/so-su.cpp
@@ -1,5 +1,6 @@
 #include <GL/glut.h>
 #include <stdio.h>
+#include <math.h>
 /*
 * そろそろここに地面生成や物体生成を別のクラスにしていきたい。
 */
@@ -10,7 +11,23 @@ const int PosX = 100;  //生成するウィンドウ位置のX座標
 const int PosY = 100;  //生成するウィンドウ位置のY座標
 const int Width = 1024;    //生成するウィンドウの幅
 const int Height = 576;    //生成するウィンドウの高さ
-float camPosX = -100;
+
+const float MoveSpeed = 0.1f;
+
+float camPosX = 0;
+float camPosZ = 200;
+
+float camRttX = 0;
+float camRttY = 0;
+
+#define PRESSED_KEY_W Prsd_W
+#define PRESSED_KEY_A Prsd_A
+#define PRESSED_KEY_S Prsd_S
+#define PRESSED_KEY_D Prsd_D
+bool Prsd_W = false;
+bool Prsd_A = false;
+bool Prsd_S = false;
+bool Prsd_D = false;
 
 int face[][4] = {//面の定義
   { 0, 1, 2, 3 },
@@ -38,21 +55,41 @@ struct Material {		//structの後の名前は自由に変えられる
 	GLfloat shininess;	//ハイライトの大きさ
 };
 
-int btime = 0;
-int ttltime = 0;
-int flame;
-void FPS() {
-	flame++;
-	int time = glutGet(GLUT_ELAPSED_TIME);
-	//printf("1フレーム0.00%d秒\n", time - btime);
-	ttltime += time - btime;
-	if (ttltime >= 1000)
+void MoveCamera()
+{
+	if (PRESSED_KEY_W)
 	{
-		printf("%dfps\n",flame);
-		ttltime -= 1000;
-		flame = 0;
+		camPosX += MoveSpeed * cos(camRttX);
+		camPosZ -= MoveSpeed * sin(camRttX);
 	}
-	btime = time;
+	if (PRESSED_KEY_A)
+	{
+		camPosX -= MoveSpeed * sin(camRttX);
+		camPosZ -= MoveSpeed * cos(camRttX);
+	}
+	if (PRESSED_KEY_S)
+	{
+		camPosX -= MoveSpeed * cos(camRttX);
+		camPosZ += MoveSpeed * sin(camRttX);
+	}
+	if (PRESSED_KEY_D)
+	{
+		camPosX += MoveSpeed * sin(camRttX);
+		camPosZ += MoveSpeed * cos(camRttX);
+	}
+}
+
+/// <summary>
+/// マウスの位置を取得
+/// </summary>
+/// <param name="x"></param>
+/// <param name="y"></param>
+int px = NULL, py = NULL;
+void MousePosition(int x, int y)
+{
+	printf("x:%d y:%d\n", x, y);
+		camRttX = (float)((Width / 2) - x) * 0.01f, camRttY = (float)((Height / 2) - y) * 0.01f;
+		//glutWarpPointer(Width / 2, Height / 2);
 }
 
 void Ground(float x,float z,float wid) {
@@ -167,15 +204,44 @@ void CreateBox(float r, float g, float b, float x, float y, float z, float width
 /// <param name="key">入力キーの種類</param>
 /// <param name="x"></param>
 /// <param name="y"></param>
-void InputKey(unsigned char key, int x, int y) {
-	printf_s("%d\n", key);
+void PressedKey(unsigned char key, int x, int y) {
+	//printf_s("%d\n", key);
 	switch (key)
 	{
 	case KEY_ESC:
 		exit(0);
 		break;
+	case 'w':
+		PRESSED_KEY_W = true;
+		break;
+	case 'a':
+		PRESSED_KEY_A = true;
+		break;
+	case 's':
+		PRESSED_KEY_S = true;
+		break;
 	case 'd':
-		camPosX += 0.5f;
+		PRESSED_KEY_D = true;
+		break;
+	default:
+		break;
+	}
+}
+
+void UpedKey(unsigned char key, int x, int y) {
+	switch (key)
+	{
+	case 'w':
+		PRESSED_KEY_W = false;
+		break;
+	case 'a':
+		PRESSED_KEY_A = false;
+		break;
+	case 's':
+		PRESSED_KEY_S = false;
+		break;
+	case 'd':
+		PRESSED_KEY_D = false;
 		break;
 	default:
 		break;
@@ -186,7 +252,6 @@ void InputKey(unsigned char key, int x, int y) {
 /// アイドル時に呼び出される関数。再描画する処理を入れている。
 /// </summary>
 void Idle() {
-	FPS();
 	glutPostRedisplay();//再描画する
 }
 
@@ -208,7 +273,6 @@ void Init() {
 /// ここに描画させたいものを描く
 /// </summary>
 void Display() {
-	
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	glMatrixMode(GL_PROJECTION);//行列モードの設定（GL_PROJECTION : 透視変換行列の設定、GL_MODELVIEW：モデルビュー変換行列）
@@ -217,9 +281,11 @@ void Display() {
 	//gluPerspective(視野角, 横割縦, 描画される最も距離, 描画される最も遠い距離);
 	gluPerspective(30.0, (double)Width / (double)Height, 0.1, 1000.0); //透視図法
 
+	MoveCamera();
+
 	gluLookAt(
-		camPosX, 50, 200, // 視点の位置x,y,z;(横、高さ、奥行)
-		camPosX, 20, 00,   // 視界の中心位置の参照点座標x,y,z(どこの座標を画面の真ん中に来るのか)
+		camPosX, 50, camPosZ, // 視点の位置x,y,z;(横、高さ、奥行)
+		camPosX + cos(camRttX) * cos(camRttY), 50 + sin(camRttY), camPosZ - sin(camRttX) * cos(camRttY),   // 視界の中心位置の参照点座標x,y,z(どこの座標を画面の真ん中に来るのか)
 		0.0, 1.0, 0.0);  //どこの座標が画面の上に来るのか(この場合+z軸が画面の上)
 
 	glMatrixMode(GL_MODELVIEW);
@@ -247,12 +313,14 @@ int main(int argc, char* argv[]) {
 
 	glutInitWindowPosition(PosX, PosY);//ウィンドウの表示位置
 	glutInitWindowSize(Width, Height); //ウィンドウサイズ
-	glutCreateWindow("ウィンドウのタイトル");  //ウィンドウの作成、タイトルを決める
+	glutCreateWindow("一人称視点");  //ウィンドウの作成、タイトルを決める
 
 	glutInitDisplayMode(GLUT_RGBA | GLUT_DEPTH | GLUT_DOUBLE);//ディスプレイモードの指定
 
 	glutDisplayFunc(Display); //Display関数に描かれているものを描画する
-	glutKeyboardFunc(InputKey);//ケー入力時にInputKeyを呼ぶ
+	glutPassiveMotionFunc(MousePosition);
+	glutKeyboardFunc(PressedKey);//ケー入力時にInputKeyを呼ぶ
+	glutKeyboardUpFunc(UpedKey);
 	glutIdleFunc(Idle);//アイドル時にIdle関数を呼ぶ
 	Init(); //初期化
 	glutMainLoop();
